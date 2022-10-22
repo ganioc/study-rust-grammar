eae1d47f1b3c93077dc0ce2ebb323e66e5de82f3
yango <o3.yang@hotmail.com>
2022-10-22 19:38:25 +0800

    Finish chap7 io

--- project/src/io.rs
+++ project/src/io.rs
@@ -1,6 +1,8 @@
 
 use std::io::{Read, Write};
 use std::net::{ Shutdown, TcpListener, TcpStream};
+use std::io::{ BufReader, BufWriter};
+use std::fs::File;
 
 
 pub fn read_it(){
@@ -53,3 +55,74 @@ pub fn run_main_client() -> std::io::Result<()> {
         }
     }
 }
+
+fn write(content: &str, file: impl Write) -> std::io::Result<()>{
+    let mut buf_writer = BufWriter::new(file);
+    buf_writer.write(content.as_bytes())?;
+    Ok (())
+}
+fn read(file: impl Read) -> std::io::Result<String>{
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents)?;
+    Ok(contents)
+}
+
+pub fn run_main_file() -> std::io::Result<()>
+{
+    let content = "Hello";
+    {
+        let file = File::create("testfile.txt")?;
+        write(content, file)?;
+    }
+    // running out of the scope will close the file
+    {
+        let file = File::open("testfile.txt")?;
+        let outcome = read(file)?;
+        assert_eq!(content, outcome);
+    }
+    // running out of the scope will close the file
+    Ok(())
+}
+
+#[derive(Debug)]
+struct Account{
+    name: String,
+    password: [u8;5] // use 32 for a 256 bit hash(SHA-256)
+}
+
+pub fn run_formatting(){
+    println!("My name is {name}", name="Arthur Dent");
+    println!("Pi = {pi:30.10}", pi=std::f64::consts::PI);
+    println!("blog.x{:x}.xyz", 1535);
+    println!("{:?}", vec!["hello", "world"]);
+
+    let me = Account{ name: "Arthur".to_string(),
+            password: [0;5]};
+    let output = format!("{:?}", me);
+
+    assert_eq!(output, "Account { name: \"Arthur\", password: [0, 0, 0, 0, 0] }");
+
+}
+
+#[allow(dead_code)]
+use std::fmt;
+struct YAccount{
+    name: String,
+    password: [u8;5]
+}
+impl fmt::Display for YAccount {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result{
+        write!(f, "Account ({name})", name = self.name)
+        // Ok(())
+    }
+    
+}
+
+pub fn run_main_account(){
+    let me = YAccount{ name: "Arthur".to_string(),
+        password: [0;5]};
+    let output = format!("{}", me);
+    assert_eq!(output, "Account (Arthur)");
+}
+
--- project/src/main.rs
+++ project/src/main.rs
@@ -5,8 +5,11 @@ mod back_of_house;
 mod io;
 
 use back_of_house::print_back_house;
-use front_of_house::{hosting, hosting::inner_hosting};
-use io::{ read_it, run_main_client, run_main_svr};
+use front_of_house::{ hosting::inner_hosting};
+use io::{ read_it, run_main_client, run_main_svr, run_main_file, run_formatting, run_main_account};
+use std::env;
+
+
 
 fn main() {
     println!("Hello, world!");
@@ -14,13 +17,20 @@ fn main() {
 
     let mut arguments = Vec::new();
 
+    let path = env::var("PATH").expect("PATH variable not found.");
+    let paths = path.split(".").collect::<Vec<_>>();
+    for item in paths {
+        println!("{}", item);
+    }
+
+
     println!("arguments len: {}", arguments.len());
 
     for argument in std::env::args() {
         println!("*** args = {}", argument);
         arguments.push(argument);
     }
-    if(arguments.len()< 2){
+    if arguments.len()< 2 {
         arguments.push(str_nonsense);
     }
     println!("1st argument: {}", arguments[1]);
@@ -37,15 +47,24 @@ fn main() {
     // print_prj1();
     read_it();
 
-    if(arguments[1].eq("client")){
+    if arguments[1].eq("client") {
         println!("It's a client");
         run_main_client();
-    }else if(arguments[1].eq("server")){
+    }else if arguments[1].eq("server") {
         println!("It's a server");
         run_main_svr();
-    }else{
+    }else if arguments[1].eq("file") {
+        println!("Running file operations");
+        run_main_file();
+    }else if arguments[1].eq("formatting") {
+        println!("Running file formatting");
+        run_formatting();
+        run_main_account();
+    }
+    else{
         println!("Unknown arguments: {}", arguments[1]);
     }
 
     
+
 }
