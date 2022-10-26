72c75660b607b0628bf7f872cc4b6316786d913a
yango3 <o3.yang@hotmail.com>
2022-10-26 17:07:17 +0800

    update mcacro

--- project/src/mcaro.rs
+++ project/src/mcaro.rs
@@ -1,3 +1,5 @@
+use std::collections::{BTreeSet, HashSet, BTreeMap};
+
 
 macro_rules! two{
     () => {1 + 1}
@@ -29,11 +31,43 @@ macro_rules! handler {
 
 handler!(stauts_handler, { Response(200) });
 
+macro_rules! set {
+    ( $($item: expr), *) => {
+        {// a nested scope to isolate the generated code,
+        let mut s = BTreeSet::new();
+        $(s.insert($item);)*
+        s
+        }
+    };
+}
+
+macro_rules! key_value {
+    ($cls:ty, $( $key:expr => $value:expr),* ) => {
+        {
+            let mut s = <$cls>::new();
+            $(s.insert($key, $value);)*
+            s
+        }
+    };
+}
+
+#[proc_macro]
+pub fn print_info(_item:TokenStream) -> TokenStream{
+    "fn print_info() {println!(\"Hello!\");}".parse().unwrap()
+}
+
 pub fn run_mcaro_main(){
     println!("two is: {}", two!());
     println!("two calc is : {}", calc!(two));
     println!("three calc is : {}", calc!(three));
     println!("repeat n times {:?}", repeat_n_times!(3, "Hello"));
     println!("status handler {:?}", stauts_handler());
+
+
+    let actual = set!("a", "b", "c", "a");
+    println!("actual: {:?}", actual);
+
+    let actual_k = key_value!(BTreeMap<&str, usize>, "Hello" => 2, "word" => 1);
+    println!("actual_k: {:?}", actual_k);
 }
 
