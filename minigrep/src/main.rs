afe6274844c0b3a7eda1ded0806af633c50cdd89
yango <o3.yang@hotmail.com>
2022-11-13 23:00:11 +0800

    update minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -3,7 +3,7 @@ use std::{env, fs};
 // % cargo run -- searchstring poem.txt
 
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, world! ====>");
     let args: Vec<String> = env::args().collect();
 
     // dbg!(args);
@@ -11,6 +11,7 @@ fn main() {
     let query = &args[1];
     let file_path = &args[2];
 
+    println!("Searching for {}", query);
     println!("In file {}", file_path);
 
     let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
