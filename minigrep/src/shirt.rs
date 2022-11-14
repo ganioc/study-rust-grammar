6c466c2fc67a9260d5db2e3276dcd324b15ccc2b
yango <o3.yang@hotmail.com>
2022-11-14 23:33:20 +0800

    update Box

--- minigrep/src/shirt.rs
+++ minigrep/src/shirt.rs
@@ -11,6 +11,8 @@
 //     Nil,
 // }
 
+use std::ops::Deref;
+
 // use crate::List::{Cons, Nil};
 enum List{
     Cons(i32, Box<List>),
@@ -56,6 +58,20 @@ impl Inventory {
 struct Rectangle {
     width: u32,
     height: u32,
+}
+// to hodl values of any type, 
+struct MyBox<T>(T);
+impl <T> MyBox<T> {
+
+    fn new(x: T)-> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
 }
 
 pub fn shirt_run() {
@@ -125,6 +141,14 @@ pub fn shirt_run() {
     println!("b = {}", b);
 
     // let list = Cons(1, Box::new(Cons(2,Box::new(Nil))));
+
+    let x = 5;
+    let y = &x;
+    assert_eq!(5,x);
+    assert_eq!(5, *y);
+
+    let z = MyBox::new(x);
+    assert_eq!(5, *z);
 
 
 }
